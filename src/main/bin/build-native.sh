dc4a178f920746a97d38cf78b1c286055e08a00f
Vladimir <bogdad@gmail.com>
2012-06-20 15:01:56 +0400

    fix to build + deleted files

--- src/main/bin/build-native.sh
+++ src/main/bin/build-native.sh
@@ -2,12 +2,11 @@
 g++ -shared -o target/lib/libcocaine-framework-java.so -Wl,-soname,libcocaine-framework-java\
 	-I${JAVA_HOME}/include \
 	-I${JAVA_HOME}/include/linux \
-	-I/usr/local/include 	\
 	-I/usr/include	\
-	-Isrc/main/cpp/include  \
+	-Isrc/main/cpp  \
 	-Itarget/generated 	\
 	-fpermissive -fPIC	 	\
-	 src/main/cpp/impl/client.cpp  src/main/cpp/impl/response.cpp src/main/cpp/impl/util.cpp\
+	 src/main/cpp/client.cpp  src/main/cpp/response.cpp src/main/cpp/util.cpp\
 	-L/usr/local/lib	\
 	-lcocaine-dealer -lc \
 	
