db0a34d7d60ff1329a7bf21d11492ca117f892f7
Vladimir <bogdad@gmail.com>
2012-06-22 12:14:47 +0400

    changes to manifest

--- src/test/resources/deploy_app.sh
+++ src/test/resources/deploy_app.sh
@@ -2,11 +2,11 @@
 
 prevdir=`pwd`
 cd src/test/resources
-tar czf package.tar.gz test.pl
+tar czf package.tar.gz --force-local test.pl
 cd $prevdir
+rm -rf ./target/cocaine/data
 LD_PRELOAD=/usr/lib/libperl.so:/usr/lib/libpython2.7.so cocaine-deploy \
 	-m $prevdir/src/test/resources/perl_test1.app.manifest \
 	-p $prevdir/src/test/resources/package.tar.gz perl_test1 \
 	-c $prevdir/src/test/resources/cocaine.conf 
-rm -rf ./target/data/cache/
 rm -rf src/test/resources/package.tar.gz
