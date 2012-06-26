886e3cb011259a8c69938937198b6331a49c869b
Vladimir <bogdad@gmail.com>
2012-06-26 08:39:18 +0000

    changes to build process

--- src/main/bin/build-native.sh
+++ src/main/bin/build-native.sh
@@ -5,13 +5,19 @@ case `uname` in
     *) SHARED_FLAG=-shared ;;
 esac
 
-if [  ! -d "${JAVA_HOME}/include" ];
-       then
-             echo "$JAVA_HOME/include does not exist, try setting JAVA_HOME to point to jdk"
-       exit 1;
+JAVAINCDIR="${JAVA_HOME}/include";
+if [  ! -d "$JAVAINCDIR" ]; then
+    echo "$JAVAINCDIR does not exist, try setting JAVA_HOME to point to jdk"
+    exit 1;
 fi
 
-g++ -$SHARED_FLAG -o target/lib/libcocaine-framework-java.so -Wl,-soname,libcocaine-framework-java\
+DEBUG_FLAG=""
+if [ "${CFJ_DEBUG+x}" = "x" ] ; then 
+    echo "building in debug mode"
+    DEBUG_FLAG="-g"
+    echo "DEBUG_FLAG is $DEBUG_FLAG"
+fi
+g++ -$SHARED_FLAG $DEBUG_FLAG -o target/lib/libcocaine-framework-java.so -Wl,-soname,libcocaine-framework-java\
 	-I/System/Library/Frameworks/JavaVM.framework/Versions/1.7/Headers \
 	-Isrc/main/cpp  			 \
 	-Itarget/generated 			 \
--- src/main/bin/run_valgrind.sh
+++ src/main/bin/run_valgrind.sh
@@ -1,1 +1,1 @@
-valgrind --trace-children=yes --show-reachable=yes --leak-check=full java -Djava.library.path=./target/lib -cp target/classes ru.yandex.cocaine.dealer.Valgrind
+java -Djava.library.path=./target/lib -cp target/classes ru.yandex.cocaine.dealer.Valgrind
