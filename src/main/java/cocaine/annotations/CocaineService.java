f209cae0f72c18db404944481671b16c66edf6b7
Anton Bobukh <abobukh@yandex-team.ru>
2013-06-14 15:53:03 +0400

    Cocaine annotations for CocaineApp, CocaineService and CocaineMethod

---
+++ src/main/java/cocaine/annotations/CocaineService.java
@@ -0,0 +1,17 @@
+package cocaine.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Anton Bobukh <abobukh@yandex-team.ru>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface CocaineService {
+
+    String value();
+
+}
