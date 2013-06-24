3bd1a618d8d1d61aaf1ace86ce30a00a5acd5fe3
Anton Bobukh <abobukh@yandex-team.ru>
2013-06-24 15:26:49 +0400

    Unused constructors are removed

--- src/main/java/cocaine/LocatorResolveException.java
+++ src/main/java/cocaine/LocatorResolveException.java
@@ -1,6 +1,5 @@
 package cocaine;
 
-import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
 /**
@@ -11,27 +10,6 @@ public class LocatorResolveException extends CocaineException {
     private final String serviceName;
     private final SocketAddress endpoint;
     private final String reason;
-
-    public LocatorResolveException(String serviceName, String host, int port, String reason) {
-        super(serviceName + " - " +  host + ":" + port + " - " + reason);
-        this.serviceName = serviceName;
-        this.endpoint = new InetSocketAddress(host, port);
-        this.reason = reason;
-    }
-
-    public LocatorResolveException(String serviceName, SocketAddress endpoint, String reason) {
-        super(serviceName + " - " +  endpoint + " - " + reason);
-        this.serviceName = serviceName;
-        this.endpoint = endpoint;
-        this.reason = reason;
-    }
-
-    public LocatorResolveException(String serviceName, String host, int port, Throwable reason) {
-        super(serviceName + " - " +  host + ":" + port + " - " + reason.getLocalizedMessage(), reason);
-        this.serviceName = serviceName;
-        this.endpoint = new InetSocketAddress(host, port);
-        this.reason = reason.getLocalizedMessage();
-    }
 
     public LocatorResolveException(String serviceName, SocketAddress endpoint, Throwable reason) {
         super(serviceName + " - " +  endpoint + " - " + reason.getLocalizedMessage(), reason);
